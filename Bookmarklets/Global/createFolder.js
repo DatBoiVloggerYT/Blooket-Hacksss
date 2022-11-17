1ca793c5991c640f0ac6c85cd47ed7f8d669bfa3
JaydenTd∆˚˚†ß∆¬˙˚˚¨Ø <94601497+JaydenTD500@users.noreply.github.com>
2022-11-18 07:29:54 +0800

    Create createFolder.js

---
+++ Bookmarklets/Global/createFolder.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bvar%20hack%20%3D%20Object.values(document.querySelector('%23app%20%3E%20div%20%3E%20div'))%5B1%5D.children%5B1%5D%5B'_owner'%5D.stateNode%3B%0Ahack.setState(%7B%0A%20%20creatingFolder%3A%20true%0A%7D)%3B%7D)()%3B
